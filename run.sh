f2ba4075980869aca2ff4641d4fc4ab63c99c2f0
Rohith M <rohith.m068@gmail.com>
2024-01-26 18:07:18 -0600

    Changed deployment.yaml file

--- run.sh
+++ run.sh
@@ -1,17 +1,10 @@
-for param in ${PARAMETERS}; do
-  # Splitting parameter into SHELL_VAR and PARAM using cut for simplicity
-  SHELL_VAR=$(echo "$param" | cut -d ',' -f1)
-  PARAM=$(echo "$param" | cut -d ',' -f2)
-
-  # Using AWS CLI to get the parameter value with error checking
-  if PASS=$(aws ssm get-parameter --name "$PARAM" --with-decryption --query "Parameter.Value" --output text 2>/dev/null); then
-    echo "export $SHELL_VAR='$PASS'" >> /data/params
-  else
-    echo "Error retrieving parameter: $PARAM"
-    # Consider whether to exit the script or continue based on your use case
-    # exit 1
-  fi
+for param in ${PARAMETERS} ; do
+  PASS=$(aws ssm get-parameter --name $PARAM --with-decryption --query "Parameter.Value" --output text)
+  echo export $PASS>> /data/params
+#  SHELL_VAR=$(echo $param | awk -F , '{print $1}')
+#  PARAM=$(echo $param | awk -F , '{print $2}')
+#  PASS=$(aws ssm get-parameter --name $PARAM --with-decryption --query "Parameter.Value" --output text)
+#  echo export $SHELL_VAR=$PASS >> /data/params
 done
 
-# Optional: Check the content of /data/params for debugging
 cat /data/params
